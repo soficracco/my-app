964b84c5f004f2ed50555092feab80c4b078e41b
chiaranarducci <cnarducci@pioix.edu.ar>
2025-08-18 10:07:26 -0300

    segui con contador hay qye hacer css

--- src/app/contador/contador.js
+++ src/app/contador/contador.js
@@ -3,10 +3,11 @@
 import Button from "@/components/Button"
 import Input from "@/components/Input"
 import Title from "@/components/Title"
-
+import { useEffect, useState } from "react"
 export default function contadorPage(){
 
     const [cuenta, setCuenta] = useState(0)
+    const [checked, setChecked] =useState(false)
 
     useEffect(() =>{
         if (cuenta >= 20){
@@ -16,28 +17,29 @@ export default function contadorPage(){
         }
     })
 
-    function incrementar(){
-        setCuenta(cuenta+1)
-    }
+  
+    function manejarBoton(){
+        
+        if (checked){
+            setCuenta (cuenta + 1);
 
-    function decrementar(){
-        setCuenta(cuenta-1)
+        }else {
+            setCuenta(cuenta - 1 );
+        }
     }
-
     function cambioCheck(event){
-        return(event.target.checked)
+        setChecked(event.target.checked)
     }
 
     function guardarInput(event){
         setCuenta(event.target.value)
     }
-    function cambiarValor(){
-        let a = cambioCheck()
-    }
+
+   
     return(
         <>
             <Title text="Pagina contador"></Title>
-            <Button texto="Cambiar valor" funcionalidad={cambiarValor}></Button>
+            <Button texto="Cambiar valor" funcionalidad={manejarBoton}></Button>
             <Input type="checkbox" onChange={cambioCheck}></Input>
             <Input type="text" onChange={guardarInput}></Input>
         </>
